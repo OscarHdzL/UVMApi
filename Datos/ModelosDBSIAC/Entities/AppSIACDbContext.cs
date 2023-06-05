c0ce891a9e0cbc0fd8772e34bf269b23d3e9f838
Oscar Hernández <oscar.hernandez@alcegix.mx>
2023-06-05 16:03:06 -0600

    Se agregan Servicios para CatRegion y CatCampus

---
+++ Datos/ModelosDBSIAC/Entities/AppSIACDbContext.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datos.ModelosDBSIAC.Entities;
+
+public partial class AppSIACDbContext : DbContext
+{
+    public AppSIACDbContext()
+    {
+    }
+
+    public AppSIACDbContext(DbContextOptions<AppSIACDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<CatCampus> CatCampuses { get; set; }
+
+    public virtual DbSet<CatRegion> CatRegions { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=PL-790;Initial Catalog=DBSIAC-Desa-UVM;Persist Security Info=False;User ID=ohl;Password=Passw0rd;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=True;Connection Timeout=30;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<CatCampus>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__cat_Camp__3214EC0701C6912F");
+
+            entity.ToTable("cat_Campus", tb => tb.HasComment("Catálogo de campus."));
+
+            entity.Property(e => e.Id).HasComment("Clave única del campus. ");
+            entity.Property(e => e.Activo).HasComment("Indica si el registro se encuentra activo en el sistema.");
+            entity.Property(e => e.FechaCreacion)
+                .HasComment("Fecha en la que fue creado el registro.")
+                .HasColumnType("datetime");
+            entity.Property(e => e.FechaModificacion)
+                .HasComment("Fecha de última modificación del registro.")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(500)
+                .HasComment("Nombre del campus.");
+            entity.Property(e => e.RegionId).HasComment("Región a la que pertenece este campus.");
+            entity.Property(e => e.UsuarioCreacion)
+                .HasMaxLength(50)
+                .HasComment("Usuario que generó el registro.");
+            entity.Property(e => e.UsuarioModificacion)
+                .HasMaxLength(50)
+                .HasComment("Usuario de última modificación del registro.");
+
+            entity.HasOne(d => d.Region).WithMany(p => p.CatCampuses)
+                .HasForeignKey(d => d.RegionId)
+                .HasConstraintName("FK_cat_Campus_cat_Region");
+        });
+
+        modelBuilder.Entity<CatRegion>(entity =>
+        {
+            entity.ToTable("cat_Region", tb => tb.HasComment("Catálogo de regiones."));
+
+            entity.Property(e => e.Id).HasComment("Clave única del región. ");
+            entity.Property(e => e.Activo).HasComment("Indica si el registro se encuentra activo en el sistema.");
+            entity.Property(e => e.FechaCreacion)
+                .HasComment("Fecha en la que fue creado el registro.")
+                .HasColumnType("datetime");
+            entity.Property(e => e.FechaModificacion)
+                .HasComment("Fecha de última modificación del registro.")
+                .HasColumnType("datetime");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(500)
+                .HasComment("Nombre del región.");
+            entity.Property(e => e.UsuarioCreacion)
+                .HasMaxLength(50)
+                .HasComment("Usuario que generó el registro.");
+            entity.Property(e => e.UsuarioModificacion)
+                .HasMaxLength(50)
+                .HasComment("Usuario de última modificación del registro.");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
